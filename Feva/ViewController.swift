e3a38f290755e7d96ae531f7dbfa2f9a68422e3f
Samuel Rockson <you@yourdomain.com>
2018-08-28 11:04:01 +0800

    Initial Commit

---
+++ Feva/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feva
+//
+//  Created by Rock on 2018/8/28.
+//  Copyright © 2018 RockzAppStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
